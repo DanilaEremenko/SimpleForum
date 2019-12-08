1a36b2643ede5170e8e14b1634fdf732f897e227
DanilaEremenko <danilaeremenko@mail.ru>
2019-12-09 00:25:23 +0300

    SimpleForum: server too stupid mistake fixed

--- run_server.py
+++ run_server.py
@@ -2,7 +2,7 @@ from lib.CommonConstants import BUFFER_SIZE
 import socket
 from lib import PacketProcessor
 from lib import Opcodes
-from threading import Thread
+from threading import Thread, Lock
 
 
 class Client():
@@ -14,36 +14,43 @@ class Client():
         self.thread = thread
 
 
-def client_processing(client: Client, client_list):
+def remove_client(client, client_list: list):
+    print("DISCONNECTING:Client = %s" % client.name)
+    send_packet = PacketProcessor.get_msg_disc()
+    client.conn.send(send_packet)
+    client.is_connected = False
+    client_list.remove(client)
+    client.conn.close()
+
+
+def client_processing(client: Client, client_list, mutex):
     client.is_connected = True
-    print("New client = %s accepted" % (client.name))
+    print("New client = %s(%d) accepted" % (client.name, client.conn.fileno()))
     while client.is_connected:
-        opcode, data = PacketProcessor.parse_packet(conn.recv(BUFFER_SIZE))
+        opcode, data = PacketProcessor.parse_packet(client.conn.recv(BUFFER_SIZE))
 
         if opcode == Opcodes.OP_MSG:
-            print("MSG from %s: %s" % (client.name, data["text"]))
-            if not data:
-                client_list.__delitem__(client)
-            if data == '':
-                print("empty data")
-                break
 
-            send_packet = PacketProcessor.get_msg_packet(client_name=client.name, text=data["text"])
-            for other_client in client_list:
-                if other_client != client:
-                    print("RESENDING TO %s" % other_client.name)
-                    other_client.conn.send(send_packet)
+            if not data or data == '':
+                remove_client(client, client_list)
+            else:
+                print("--------------\nMSG from %s: %s" % (client.name, data["text"]))
+
+                send_packet = PacketProcessor.get_msg_packet(client_name=client.name, text=data["text"])
+
+                mutex.acquire()
+                for other_client in client_list:
+                    if other_client != client:
+                        print("RESENDING TO %s" % other_client.name)
+                        other_client.conn.send(send_packet)
+                mutex.release()
 
         elif opcode == Opcodes.OP_DISC:
-            print("DISCONNECTING:Client = %s" % client.name)
-            send_packet = PacketProcessor.get_msg_disc()
-            conn.send(send_packet)
-            client.is_connected = False
+            remove_client(client, client_list)
 
         else:
             raise Exception("Undefined opcode = %d" % opcode)
 
-    client.conn.close()
     print("DISCONNECTED:Client = %s" % client.name)
 
 
@@ -58,7 +65,7 @@ def cmd_processing(client_list):
         print("-------------------------")
 
 
-if __name__ == '__main__':
+def main():
     client_list = []
 
     TCP_IP = 'localhost'
@@ -68,6 +75,8 @@ if __name__ == '__main__':
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((TCP_IP, TCP_PORT))
     s.listen(1)
+
+    mutex = Lock()
 
     # running cmd thread
     cmd_thread = Thread(target=cmd_processing, args=(client_list,), daemon=True)
@@ -80,9 +89,17 @@ if __name__ == '__main__':
         if opcode == Opcodes.OP_MSG:
             name = data["client_name"]
             new_client = Client(conn=conn, addr=addr, name=name, thread=None)
-            new_client.thread = Thread(target=client_processing, args=(new_client, client_list), daemon=True)
+            new_client.thread = Thread(target=client_processing, args=(new_client, client_list, mutex), daemon=True)
+
+            mutex.acquire()
             client_list.append(new_client)
+            mutex.release()
+
             new_client.thread.start()
 
         else:
             print("opcode = %d (%d awaiting)" % (opcode, Opcodes.OP_MSG))
+
+
+if __name__ == '__main__':
+    main()
